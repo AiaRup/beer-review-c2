3712de37364662f3bf46b3d6f9d59c97bfeb61d8
AiaRup <aiale88@gmail.com>
2018-04-09 12:31:35 +0300

    finished with MODULAR DESIGN

--- script.js
+++ script.js
@@ -1,63 +1,44 @@
-// PROJECT EXERCISE 1, 2, 3, 4
-var beers = [];
+// MODULAR DESIGN
+var BeerReviewApp = function() {
+  var beers = [];
 
-var addBeer = function(name, category, rate) {
-  var newBeer = {
-    beerName: name,
-    beerCategory: category,
-    beerRate: rate
+  var addBeer = function(name, category, rate) {
+    var newBeer = {
+      beerName: name,
+      beerCategory: category,
+      beerRate: rate
+    };
+    beers.push(newBeer);
   };
-  beers.push(newBeer);
+
+  var updateBeers = function() {
+    $('li').remove();
+    for (var i = 0; i < beers.length; i++) {
+      $('.beers-list').append(
+        '<li>Name: ' +
+          beers[i].beerName +
+          ' , Category:' +
+          beers[i].beerCategory +
+          ' , Rate: ' +
+          beers[i].beerRate +
+          '</li>'
+      );
+    }
+  };
+
+  return {
+    addBeer: addBeer,
+    updateBeers: updateBeers
+  };
 };
 
-var renderBeers = function() {
-  $('li').remove();
-  for (var i = 0; i < beers.length; i++) {
-    $('.beers-list').append(
-      '<li>Name: ' +
-        beers[i].beerName +
-        ' , Category:' +
-        beers[i].beerCategory +
-        ' , Rate: ' +
-        beers[i].beerRate +
-        '</li>'
-    );
-  }
-};
+var beerReviewApp = BeerReviewApp();
 
+// Eventhandler for post button - no sorting
 $('.post-beer').on('click', function() {
   var name = $('#beer-name').val();
   var category = $('#beer-category').val();
   var rate = $('.custom-select').val();
-  addBeer(name, category, rate);
-  renderBeers();
+  beerReviewApp.addBeer(name, category, rate);
+  beerReviewApp.updateBeers();
 });
-
-// PROJECT EXERCISE 5 - sort button
-var clicks = 0;
-$('.sort').on('click', function() {
-  $('li').remove();
-  if(clicks % 2 == 0) {
-    beers.sort(sortASE);
-    renderBeers();
-  }
-  else {
-    beers.sort(sortDES);
-    renderBeers();
-  }
-  clicks++;
-});
-
-// sort in ascending order
-function sortASE(a,b) {
-  return (a.beerName > b.beerName) ? 1 : ((b.beerName > a.beerName) ? -1 : 0);
-} 
-
-// sort in descending order
-function sortDES(a,b) {
-  return (a.beerName > b.beerName) ? -1 : ((b.beerName > a.beerName) ? 1 : 0);
-} 
-
-
-
-
